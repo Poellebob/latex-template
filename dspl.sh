734c83f68e216cca70361c9a56d68ae0480e8a66
Viggo Kirkegaard Helstrup <vkirkegaardhelstrup@gmail.com>
2026-03-03 08:54:31 +0100

    download spell file

---
+++ dspl.sh
@@ -0,0 +1,10 @@
+lang="$1"
+prv="$PWD"
+
+mkdir -p "$HOME/.local/share/nvim/site/spell"
+cd "$HOME/.local/share/nvim/site/spell" || exit 1
+
+curl -O "https://ftp.nluug.nl/pub/vim/runtime/spell/${lang}.utf-8.spl"
+curl -O "https://ftp.nluug.nl/pub/vim/runtime/spell/${lang}.utf-8.sug"
+
+cd "$prv" || exit 1
