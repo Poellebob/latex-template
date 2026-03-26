454a24bd53ea646b5d5637577cc0bc414bd8497d
Poellebob <91906512+Poellebob@users.noreply.github.com>
2026-03-26 08:52:05 +0100

    rollback to nuugl mirrors

--- dspl.sh
+++ dspl.sh
@@ -4,7 +4,7 @@ prv="$PWD"
 mkdir -p "$HOME/.local/share/nvim/site/spell"
 cd "$HOME/.local/share/nvim/site/spell" || exit 1
 
-curl -O "https://mirrors.kernel.org/vim/runtime/spell/${lang}.utf-8.spl"
-curl -O "https://mirrors.kernel.org/vim/runtime/spell/${lang}.utf-8.sug"
+curl -O "https://ftp.nluug.nl/pub/vim/runtime/spell/${lang}.utf-8.spl"
+curl -O "https://ftp.nluug.nl/pub/vim/runtime/spell/${lang}.utf-8.sug"
 
 cd "$prv" || exit 1
